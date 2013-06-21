49252ce9479a57841a6e8067238bce08e233c592
Stephen Commisso <scommisso@godaddy.com>
2013-06-20 22:09:19 -0700

    Updated assemblyInfo / added SharedAssemblyInfo links

--- PromoStudio.Storage/Properties/AssemblyInfo.cs
+++ PromoStudio.Storage/Properties/AssemblyInfo.cs
@@ -6,31 +6,6 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("PromoStudio.Storage")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("PromoStudio.Storage")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
+[assembly: AssemblyDescription("Contains classes relating to storing PromoStudio videos.")]
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("d92955c0-0729-4b06-a0d7-4db58191da18")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
--- PromoStudio.Web/Properties/AssemblyInfo.cs
+++ PromoStudio.Web/Properties/AssemblyInfo.cs
@@ -5,8 +5,8 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("PromoStudio.RenderQueueService")]
-[assembly: AssemblyDescription("The PromoStudio rendering service.")]
+[assembly: AssemblyTitle("PromoStudio.Web")]
+[assembly: AssemblyDescription("The PromoStudio web application.")]
 [assembly: ComVisible(false)]
 [assembly: Guid("167b28fe-46c8-41ce-bb3f-82df1656e16f")]
 
